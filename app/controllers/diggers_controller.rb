f74ba866440d73147200022dfcc42eda26dd8b5e
Carl Bentham <carlbentham@gmail.com>
2019-08-19 18:02:29 +0200

    homepage date search

---
+++ app/controllers/diggers_controller.rb
@@ -0,0 +1,14 @@
+class DiggersController < ApplicationController
+  def index
+    if params[:start_date]
+      start_date = Date.civil(params[:start_date][:year].to_i,
+                 params[:start_date][:month].to_i,
+                 params[:start_date][:day].to_i)
+      end_date = Date.civil(params[:end_date][:year].to_i,
+                 params[:end_date][:month].to_i,
+                 params[:end_date][:day].to_i)
+      @diggers = Digger.where("start_date <= :sd AND end_date >= :ed",
+                              sd: start_date, ed: end_date)
+    end
+  end
+end

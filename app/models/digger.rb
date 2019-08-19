30512673b804fc89eef862dc792e4015b37d974b
Max Würfel <maxwuerfel24@gmail.com>
2019-08-19 18:00:45 +0200

    search and show

--- app/models/digger.rb
+++ app/models/digger.rb
@@ -1,4 +1,8 @@
 class Digger < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  def self.search(search)
+    where("name ILIKE ? OR technical_specification ILIKE ?", "%#{search}%", "%#{search}%")
+  end
 end

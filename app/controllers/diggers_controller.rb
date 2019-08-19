5897885c7bc719d5c62f8ff47c9fc1fc4b034fd1
Niklas Teichmann <niklas2106@googlemail.com>
2019-08-19 18:18:59 +0200

    Create and Edit erea for diggers

---
+++ app/controllers/diggers_controller.rb
@@ -0,0 +1,36 @@
+class DiggersController < ApplicationController
+  def new
+  @digger = Digger.new
+  end
+
+  def create
+    @digger = Digger.new(digger_params)
+    @user = current_user.id
+    @digger.user_id = @user
+    if @digger.save
+      redirect_to diggers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @digger = Digger.find(params[:id])
+  end
+
+  def update
+    @digger = Digger.find(params[:id])
+    if @digger.update(digger_params)
+      redirect_to diggers_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def digger_params
+    params.require(:digger).permit(:name, :technical_specification, :start_date,
+    :end_date, :photo, :day_rate)
+  end
+end
--- app/models/digger.rb
+++ app/models/digger.rb
@@ -1,4 +1,7 @@
 class Digger < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
+  validates :technical_specification, presence: true
+  validates :day_rate, numericality: true, presence: true
 end

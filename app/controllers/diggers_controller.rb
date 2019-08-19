282a87ab9ea49a2ec8333119c2bd341a2fe71615
Carl Bentham <carlbentham@gmail.com>
2019-08-19 18:53:05 +0200

    more tweaks

--- app/controllers/diggers_controller.rb
+++ app/controllers/diggers_controller.rb
@@ -1,18 +1,18 @@
 class DiggersController < ApplicationController
   def index
-    if params[:start_date]
-      start_date = Date.civil(params[:start_date][:year].to_i,
-                 params[:start_date][:month].to_i,
-                 params[:start_date][:day].to_i)
-      end_date = Date.civil(params[:end_date][:year].to_i,
-                 params[:end_date][:month].to_i,
-                 params[:end_date][:day].to_i)
+    start_date = Date.civil(params[:start_date][:year].to_i,
+                            params[:start_date][:month].to_i,
+                            params[:start_date][:day].to_i)
+    end_date = Date.civil(params[:end_date][:year].to_i,
+                          params[:end_date][:month].to_i,
+                          params[:end_date][:day].to_i)
+    if dates_validate?(start_date, end_date)
       @diggers = Digger.where("start_date <= :sd AND end_date >= :ed",
                               sd: start_date, ed: end_date)
     elsif params[:search]
       @diggers = Digger.search(params[:search])
     else
-      @diggers = Digger.all
+      render "pages/home"
     end
   end
 
@@ -21,5 +21,11 @@ class DiggersController < ApplicationController
 
   def show
     @digger = Digger.find(params[:id])
+  end
+
+  private
+
+  def dates_validate?(start, end_d)
+    true if start <= end_d && params[:start_date] && params[:end_date]
   end
 end

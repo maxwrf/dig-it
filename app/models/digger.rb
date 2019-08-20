ab107d7ca496f6adbe0dbebc339a1f47947559a6
Max Würfel <maxwuerfel24@gmail.com>
2019-08-20 08:55:32 +0200

    photo upload

--- app/models/digger.rb
+++ app/models/digger.rb
@@ -2,6 +2,8 @@ class Digger < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  mount_uploader :photo, PhotoUploader
+
   def self.search(search)
     where("name ILIKE ? OR technical_specification ILIKE ?", "%#{search}%", "%#{search}%")
   end

912d3f4c43e36e94bb112d58a51e02c1f5ff247c
Carl Bentham <carlbentham@gmail.com>
2019-08-20 14:02:05 +0200

    mid-way, pre-merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    # probs takes first 8 (thought technically not ordered
+    # TODO: add 'featured' attr to diggers more manual curation
+    # raise
+    @diggers = Digger.take(8)
+    #@diggers = [1]
 
   end
 end

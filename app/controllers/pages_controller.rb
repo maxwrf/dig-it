0bd4ebe0a91d09a8aca8ad367057f6a6b122d210
Carl Bentham <carlbentham@gmail.com>
2019-08-20 15:58:27 +0200

    banner, and featured diggers added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,6 @@ class PagesController < ApplicationController
   def home
     # probs takes first 8 (thought technically not ordered
     # TODO: add 'featured' attr to diggers more manual curation
-    # raise
     @diggers = Digger.take(8)
-    #@diggers = [1]
-
   end
 end

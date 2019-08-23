b66d947dcf2078368433191e1b43d2f30035c179
Max Würfel <maxwuerfel24@gmail.com>
2019-08-23 11:38:35 +0200

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'ws://www.can-i-dig-it.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://www.can-i-dig-it.herokuapp.com', 'https://www.can-i-dig-it.herokuapp.com' ]
+  config.action_cable.url = 'wss://www.can-i-dig-it.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://www.can-i-dig-it.herokuapp.com', 'https://www.can-i-dig-it.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

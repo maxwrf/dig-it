b22005e533dc1b6dfc0177094335fb67a5b6d841
Max Würfel <maxwuerfel24@gmail.com>
2019-08-23 15:33:18 +0200

    yes you can

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('.col-thematic-inv', {
-    strings: ["CAN I DIG IT"],
+    strings: ["CAN I DIG IT?", "CAN I DIG IT??", "CAN I DIG IT???", "YES YOU CAN"],
     typeSpeed: 50,
     loop: true,
     smartBackspace: false,
